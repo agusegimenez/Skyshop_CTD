87a3a20b11a96f6392f1aa21b9a28947fba90040
Santioven <santioven1@gmail.com>
2024-08-08 22:01:19 -0300

    ItemService + IItemService

--- SkyShop/src/main/java/com/equipo_1/SkyShop/service/IItemService.java
+++ SkyShop/src/main/java/com/equipo_1/SkyShop/service/IItemService.java
@@ -1,4 +1,13 @@
 package com.equipo_1.SkyShop.service;
 
+import com.equipo_1.SkyShop.entity.Item;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IItemService {
+    void addItem(Item item);
+    Optional<Item> getItem(Long id);
+    List<Item> listItems();
+    void deleteItem(Long id);
 }
--- SkyShop/src/main/java/com/equipo_1/SkyShop/service/implementations/ItemService.java
+++ SkyShop/src/main/java/com/equipo_1/SkyShop/service/implementations/ItemService.java
@@ -1,4 +1,38 @@
 package com.equipo_1.SkyShop.service.implementations;
 
-public class ItemService {
+import com.equipo_1.SkyShop.entity.Item;
+import com.equipo_1.SkyShop.repository.ItemRepository;
+import com.equipo_1.SkyShop.service.IItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService implements IItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public void addItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Override
+    public Optional<Item> getItem(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    public List<Item> listItems() {
+        return itemRepository.findAll();
+    }
+
+    @Override
+    public void deleteItem(Long id) {
+        itemRepository.deleteById(id);
+    }
 }
+
