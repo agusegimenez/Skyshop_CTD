9f3b3a7d30cd8984541c8b50bfeb5607221b233d
juancruzmarzetti <naujmarz@gmail.com>
2024-08-12 23:27:41 -0300

    detail-styles

--- SkyShop/src/Routes/Detail.jsx
+++ SkyShop/src/Routes/Detail.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import customCss from "./Detail.module.css";
 
 const Detail = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const { producto } = location.state || {}; 
 
   if (!producto) {
     return <div>Producto no encontrado</div>;
   }
-  console.log(producto);
 
   const { imagen, nombre, precio, id, contenido } = producto;
 
   return (
     <section className={customCss.detailSect}>
-      <div>
-        <img src={imagen} alt={nombre} />
+      <div className={customCss.primerDiv}>
+        <div className={customCss.divNombreYBoton}>
+          <h3 className={customCss.nombreSt}>{nombre}</h3>
+          <button
+          className={customCss.btnCerrar}
+          onClick={() => navigate(-1)}>
+            <img src="/icono-flecha.png" alt="flecha atras"/>
+          </button>
+        </div>
+        <div className={customCss.imgDiv}>
+          <img src={imagen} alt={nombre} />
+        </div>
       </div>
-      <div>
-        <h3>{nombre}</h3>
-        <p>${precio}</p>
-      </div>
-      <h2>Detalles del Producto</h2>
-      <p>ID del Producto: {id}</p>
-      <ul>
+      <ul className={customCss.contenidoMod}>
         {contenido.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>{item} x1</li>
         ))}
       </ul>
+      <p className={customCss.precioP}><span>Precio </span>${precio}</p>
+      <button className={customCss.btnAgregar}>Agregar a mi pedido</button>
     </section>
   );
 };
