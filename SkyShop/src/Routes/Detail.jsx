c094bc6588019b187a83d4d4f53ad07004ebbc5d
juancruzmarzetti <naujmarz@gmail.com>
2024-08-16 00:05:14 -0300

    detail-styles-changed

--- SkyShop/src/Routes/Detail.jsx
+++ SkyShop/src/Routes/Detail.jsx
@@ -15,26 +15,37 @@ const Detail = () => {
 
   return (
     <section className={customCss.detailSect}>
-      <div className={customCss.primerDiv}>
-        <div className={customCss.divNombreYBoton}>
-          <h3 className={customCss.nombreSt}>{nombre}</h3>
-          <button
-          className={customCss.btnCerrar}
-          onClick={() => navigate(-1)}>
-            <img src="/icono-flecha.png" alt="flecha atras"/>
-          </button>
+      <div className={customCss.divGral}>
+        <div className={customCss.primerDiv}>
+          <div className={customCss.divNombreYBoton}>
+            <h3 className={customCss.nombreSt}>{nombre}</h3>
+            <button
+              className={customCss.btnCerrar}
+              onClick={() => navigate(-1)}>
+              <img src="/flechaizquierda.png" alt="flecha atras"/>
+            </button>
+          </div>
+          <div className={customCss.imgDiv}>
+            <img src={imagen} alt={nombre} />
+          </div>
         </div>
-        <div className={customCss.imgDiv}>
-          <img src={imagen} alt={nombre} />
+        <div>
+          <div className={customCss.contenidoMod}>
+            <ul>
+              {contenido.map((item, index) => (
+                <li key={index}>
+                  {item}
+                  <p>x1</p>
+                </li>
+              ))}
+            </ul>
+            <p className={customCss.precioP}><span>Precio </span>${precio}</p>
+          </div>
+          <div className={customCss.divBtn}>
+            <button className={customCss.btnAgregar}>Agregar a mi pedido</button>
+          </div>
         </div>
       </div>
-      <ul className={customCss.contenidoMod}>
-        {contenido.map((item, index) => (
-          <li key={index}>{item} x1</li>
-        ))}
-      </ul>
-      <p className={customCss.precioP}><span>Precio </span>${precio}</p>
-      <button className={customCss.btnAgregar}>Agregar a mi pedido</button>
     </section>
   );
 };
