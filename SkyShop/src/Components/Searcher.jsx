8f661f2550362ba7ee2d9f3393e7e374828194a8
juancruzmarzetti <naujmarz@gmail.com>
2024-09-01 23:31:12 -0300

    searcher-matches-added

--- SkyShop/src/Components/Searcher.jsx
+++ SkyShop/src/Components/Searcher.jsx
@@ -1,6 +1,76 @@
+import { useEffect, useRef, useState } from "react";
 import customCss from "./Searcher.module.css"
+import { productos } from "../utils/products";
 
 export default function Searcher() {
+
+  const [isTyping, setIsTyping] = useState(false);
+  const [typingContent, setTypingContent] = useState("");
+  const productNames = productos.map(product => product.nombre);
+
+  const inputRef = useRef(null);
+  const recommendationsRef = useRef(null);
+
+  const handleTypingContent = (e) => {
+    setTypingContent(e.target.value);
+  }
+
+  const mapMatches = (stringValue) => {
+    if (stringValue === "") return [];
+    const lowercasedSearchValue = stringValue.toLowerCase();
+    return productNames.filter((item) =>
+      item.toLowerCase().includes(lowercasedSearchValue)
+    );
+  };
+
+  const renderMatches = (matches) => {
+    return (
+      <div
+        className={customCss.divSearchRecs}
+        style={{ display: matches.length === 0 ? "none" : "block" }}
+        ref={recommendationsRef}
+      >
+        <div className={customCss.searchRecs}>
+          {matches.map((match, index) => (
+            <a key={index}>{match}</a>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  const handleIsTyping = (e) => {
+    const matches = mapMatches(e.target.value);
+    setIsTyping(matches.length > 0);
+  };
+
+  useEffect(() => {
+  }, [typingContent, isTyping])
+  
+
+  const handleSearch = (e) => {
+    handleIsTyping(e);
+    handleTypingContent(e);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(event.target) &&
+        recommendationsRef.current &&
+        !recommendationsRef.current.contains(event.target)
+      ) {
+        setIsTyping(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <section className={customCss.searcherSect}>
         <div className={customCss.textDiv}>
@@ -11,9 +81,12 @@ export default function Searcher() {
             en tu balcón!
           </h2>
           <div className={customCss.inputDiv}>
-            <input className={customCss.searcherInput} type="search" id="buscador"/>
-            <img src="/search.png"/>
+            <input ref={inputRef} className={customCss.searcherInput} type="search" id="buscador" onChange={(e) => handleSearch(e)}/>
+            <a>
+              <img src="/search.png"/>
+            </a>
           </div>
+          {isTyping && renderMatches(mapMatches(typingContent))}
         </div>
         <div className={customCss.divImg}>
           <img src="/man-with-drone-delivery.png" alt="Imagen de persona recibiendo delivery de drone"/>
