fdfecf0c6407e18d5745554dc3422335df00a5b8
juancruzmarzetti <naujmarz@gmail.com>
2024-09-02 23:47:13 -0300

    upload-img-new-prod-to-cloud-generating-link

--- SkyShop/src/Routes/AñadirProd.jsx
+++ SkyShop/src/Routes/AñadirProd.jsx
@@ -1,49 +1,95 @@
 import React, { useState } from 'react'
 import customCss from "./AñadirProd.module.css"
+import axios from 'axios';
 
 const AñadirProd = () => {
     const [productName, setProductName] = useState("");
     const [productDescription, setProductDescription] = useState("");
     const [productPrice, setProductPrice] = useState("");
     const [categorias, setCategorias] = useState([]);
-    const [imagenes, setImagenes] = useState([]);
+    const [imagen, setImagen] = useState(""); // estado que guarda la imagen subida al navegador
+    const [imgFile, setImgFile] = useState(null); // estado que guarda la imagen en formato archivo
+    const [imageCloudUrl, setImageCloudUrl] = useState(""); // estado q guarda el link de la url de la imagen en Cloudinary
     const [error, setError] = useState(false);
 
-    const handleImagenSubida =  (e) => {
-      const files = Array.from(e.target.files); // Convertimos los archivos en un array
-      const nuevasImagenes = files.map(file => URL.createObjectURL(file));
-      setImagenes([...imagenes, ...nuevasImagenes]);
-    };
+    const resetForm = () => {
+      setProductName("");
+      setProductDescription("");
+      setProductPrice("");
+      setCategorias([]);
+      setImagen("");
+      setImgFile(null);
+      setImageCloudUrl("");
+  };
 
-    const handleCategory = (e) => {
-        const value = e.target.value;
-        setCategorias([...categorias, value]);
+    const handleImagenSubida = (e) => {
+      const file = e.target.files[0]; // Selecciona solo el primer archivo
+      setImgFile(file);
+      const nuevaImagen = URL.createObjectURL(file);
+      setImagen(nuevaImagen); // Reemplaza la imagen anterior si la hubiera
+  };
+
+  // subida de imagen a Cloudinary
+  // (implementacion cuando se pueda agregar productos al back-end)
+  const uploadImageToCloudinary  = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'x1kdwphk');
+
+    try {
+      const response = await axios.post('https://api.cloudinary.com/v1_1/dqeczcnjq/image/upload', formData);
+      return response.data.secure_url;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      return null;
     }
+  };
+
+  const handleCategory = (e) => {
+      const value = e.target.value;
+      setCategorias([...categorias, value]);
+  }
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(productName.trim() === "") {
-            setError(true);
-        }else {
-            setError(false);
-            console.log('Producto guardado', { productName, productDescription, productPrice, categorias, imagen });
-        }
-    }
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      if(productName.trim() === "") {
+          setError(true);
+      }else {
+          setError(false);
+          let url = "";
+          // se ejecuta subida de img a Cloudinary si no hay errores y si hay una imagen seleccionada
+          if(imgFile !== null){
+            url = await uploadImageToCloudinary(imgFile);
+            if (url) {
+              setImageCloudUrl(url);
+            } else {
+              console.error("Error uploading image");
+              return;
+            }
+          }
+          if(imagen === "" || imgFile === null){
+            console.error("Error uploading image");
+          }
+          // cambiar esto con POST a API
+          console.log('Producto guardado', { productName, productDescription, productPrice, categorias, imageCloudUrl });
+          //limpiar los inputs si se guardo bien el producto
+          resetForm();
+      }
+  }
   return (
      <form onSubmit={handleSubmit} className={customCss.productForm}>
             <h2>Datos del Producto</h2>
             <label>Imagen:</label>
             <div className={customCss.imgPreview}>
                 {/* Vista previa de imágenes */}
-                {imagenes.map((img, index) => (
-                    <img key={index} src={img} alt={`Producto ${index + 1}`} className={customCss.imgThumbnail} />
-                ))}
+                {
+                  imagen !== "" && <img src={imagen} className={customCss.imgThumbnail} />
+                }
                 {/* Botón para agregar más imágenes */}
                 <label className={customCss.addImage}>
                     <input 
                         type="file" 
-                        accept='image/*' 
-                        multiple 
+                        accept='image/*'  
                         onChange={handleImagenSubida} 
                         style={{ display: 'none' }} 
                     />
