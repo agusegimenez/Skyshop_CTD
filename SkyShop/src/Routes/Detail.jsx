69f03ecce5206a03fe6092bfd33a82ca231332ad
MateoPane <mateopane04@gmail.com>
2024-09-06 18:11:08 -0300

    detail calendar

--- SkyShop/src/Routes/Detail.jsx
+++ SkyShop/src/Routes/Detail.jsx
@@ -1,65 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate,useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import customCss from "./Detail.module.css";
 import { BotonContext } from '../Context/Context';
 import { arrayToLowerCase, productos } from '../utils/products';
+import DatePicker from 'react-datepicker';
+import { es } from 'date-fns/locale';
+import "/node_modules/react-datepicker/dist/react-datepicker.css"; 
+import "./CustomDatePicker.css"
 
 const Detail = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { id } = useParams(); // id de producto
-  const { loggedUser, token} = useContext(BotonContext);
+  const { loggedUser, token, finalizarPedido } = useContext(BotonContext);
   const [producto, setProducto] = useState(null);
   const { agregarProductoAlCarrito } = useContext(BotonContext);
   const [mainImg, setMainImg] = useState(""); // estado que guarda la imagen que se muestra grande
+  const [selectedTime, setSelectedTime] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isHorarioVisible, setIsHorarioVisible] = useState(false);
   const url = "http://localhost:8080/api/items/" + id;
+
+  const toggleHorario = () => {
+    setIsHorarioVisible(!isHorarioVisible);
+  };
 
   const getDetailProd = async () => {
     const settings = {
       method: "GET",
       headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
       },
       credentials: 'include',
     }
 
-    try{
+    try {
       const response = await fetch(url, settings);
 
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error('Error al hacer peticion GET de productos');
-      }else{
+      } else {
         const data = await response.json();
         console.log("GET Producto detalle: ", data);
         setMainImg(data.images[0]);
         setProducto(data);
       }
-    }catch(error){
+    } catch (error) {
       console.error('Error en la petición GET de productos: ', err);
     }
   }
 
   const handleMainImg = (newMainImg) => {
-    if(newMainImg !== mainImg) setMainImg(newMainImg);
+    if (newMainImg !== mainImg) setMainImg(newMainImg);
   }
 
   console.log("valor de producto: " + producto);
 
-   // esta funcion recorre el array de strings y devuelve un array de strings en minusculas
-  // es para que por cada carcateristica, suponiendo q las imagenes de iconos de las caracteristicas
-  // son por ej: si la caracteristica es "Alimentos", la imagen/icono tendria q llamarse "caracteristica_alimentos.png"
-  // esto sirve para implementar la busqueda dinamica de los iconos de las caracteristicas ya que
-  // tampoco va a pasar que se puedan crear nuevas caracteristicas con nuevos iconos
-  // sino que en el agregado de productos (si es q lo implementamos) seria ideal que
-  // las caracteristicas se pudieran solo elegir tipo checkbox con un maximo de tildar de 5 opciones o algo asi
-  // y en la carpeta "public" habria un icono por cada caracteristica
-
+  // esta funcion recorre el array de strings y devuelve un array de strings en minusculas
+ // es para que por cada carcateristica, suponiendo q las imagenes de iconos de las caracteristicas
+ // son por ej: si la caracteristica es "Alimentos", la imagen/icono tendria q llamarse "caracteristica_alimentos.png"
+ // esto sirve para implementar la busqueda dinamica de los iconos de las caracteristicas ya que
+ // tampoco va a pasar que se puedan crear nuevas caracteristicas con nuevos iconos
+ // sino que en el agregado de productos (si es q lo implementamos) seria ideal que
+ // las caracteristicas se pudieran solo elegir tipo checkbox con un maximo de tildar de 5 opciones o algo asi
+ // y en la carpeta "public" habria un icono por cada caracteristica
+ 
   useEffect(() => {
-    getDetailProd()
-  }, [id])
-  
+    getDetailProd();
+  }, [id]);
 
   const mostrarCaracteristicas = () => {
     const caracteristicasLowerCase = arrayToLowerCase(producto.characteristics);
@@ -73,10 +83,13 @@ const Detail = () => {
       );
     });
   };
+
   if (!producto) {
-    return <div style={{marginTop: "200px"}}>Producto no encontrado</div>;
+    return <div style={{ marginTop: "200px" }}>Producto no encontrado</div>;
   }
+
   const { name, images, description, price } = producto;
+
   return (
     <section className={customCss.detailSect}>
       <div className={customCss.divGral}>
@@ -86,22 +99,30 @@ const Detail = () => {
             <button
               className={customCss.btnCerrar}
               onClick={() => navigate(-1)}>
-              <img src="/flechaizquierda.png" alt="flecha atras"/>
+              <img src="/flechaizquierda.png" alt="flecha atras" />
             </button>
           </div>
           <div className={customCss.imgDiv}>
             <img src={mainImg} alt={name} />
           </div>
           <div className={customCss.allImgsDiv}>
-            {images.map(imagen => <button
-              className={customCss.imgOptionBtn}
-              style={{ backgroundColor: imagen === mainImg && '#d0fdd7'}}
-              onClick={() => handleMainImg(imagen)}>
-                <img src={imagen}/>
-              </button> )}
+            {images.map(imagen => (
+              <button
+                className={customCss.imgOptionBtn}
+                style={{ backgroundColor: imagen === mainImg && '#d0fdd7' }}
+                onClick={() => handleMainImg(imagen)}>
+                <img src={imagen} />
+              </button>
+            ))}
+          </div>
+          <div className={customCss.divCaracteristicas}>
+            <h3>Características</h3>
+            <ul className={customCss.listaCaracteristicas}>
+              {mostrarCaracteristicas()}
+            </ul>
           </div>
         </div>
-        <div>
+        <div className={customCss.derechoDetail}>
           <div className={customCss.contenidoMod}>
             <ul>
               {description.split(',').map((item, index) => (
@@ -113,9 +134,48 @@ const Detail = () => {
             </ul>
             <p className={customCss.precioP}><span>Precio </span>${price}</p>
           </div>
+          <div className={customCss.calendarioPadre}>
+            <h3>Seleccione el día y hora en el que se encontrará disponible para recibir su pedido.</h3>
+          </div>
+          <div className={customCss.calendarioPadre}>
+            <div className={customCss.calendario}>
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                inline
+                locale={es}
+                dateFormat="dd/MM/yyyy"
+                className={customCss.customCalendar}
+              />
+            </div>
+          </div>
+          <div className={customCss.horarioContainer}>
+            <div className={customCss.horarioWrapper}>
+              <span className={customCss.horarioLabel}>Horario:</span>
+              <div className={customCss.horarioSelect}>
+                <span>{selectedTime || "Selecciona una hora"}</span>
+                <button onClick={toggleHorario} className={customCss.dropdownButton}>
+                  &#9660;
+                </button>
+              </div>
+            </div>
+            {isHorarioVisible && (
+              <div className={customCss.horarioDropdown}>
+                <ul className={customCss.horarioLista}>
+                  {["07:00 - 08:00", "08:00 - 09:00", "09:00 - 10:00", "10:00 - 11:00", "11:00 - 12:00", "12:00 - 13:00", "13:00 - 14:00", "14:00 - 15:00", "15:00 - 16:00", "16:00 - 17:00", "17:00 - 18:00", "18:00 - 19:00", "19:00 - 20:00", "20:00 - 21:00", "21:00 - 22:00"].map((time) => (
+                    <li key={time} className={customCss.horarioItem} onClick={() => setSelectedTime(time)}>
+                      {time}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
           <div className={customCss.divBtn}>
             {loggedUser ? (
-              <button className={customCss.btnAgregar} onClick={() => agregarProductoAlCarrito(producto)}>Agregar a mi pedido</button>
+              <button className={customCss.btnAgregar} onClick={() => agregarProductoAlCarrito(producto)}>
+                <a href="/carrito">Agregar a mi pedido</a>
+              </button>
             ) : (
               <button className={customCss.btnAgregarDisabled} disabled>
                 Debes iniciar sesión para agregar al carrito
@@ -124,13 +184,8 @@ const Detail = () => {
           </div>
         </div>
       </div>
-      <div className={customCss.divCaracteristicas}>
-        <h3>Características</h3>
-        <ul className={customCss.listaCaracteristicas}>
-          {mostrarCaracteristicas()}
-        </ul>
-      </div>
     </section>
   );
 };
+
 export default Detail;
